e0d4f800b9ecc43c032c865bfa812007fda3aab5
Andriy <kingandriy@mail.ru>
2018-06-21 16:49:54 +0300

    all file  first

---
+++ oc.loc/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.loc/admin/');
+define('HTTP_CATALOG', 'http://oc.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc.loc/admin/');
+define('HTTPS_CATALOG', 'http://oc.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/oc.loc/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/oc.loc/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/oc.loc/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/oc.loc/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/oc.loc/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/oc.loc/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/oc.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/oc.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/oc.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/oc.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/oc.loc/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/oc.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc');
+define('DB_PASSWORD', '1234');
+define('DB_DATABASE', 'oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ oc.loc/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/oc.loc/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/oc.loc/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/oc.loc/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/oc.loc/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/oc.loc/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/oc.loc/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/oc.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/oc.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/oc.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/oc.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/oc.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc');
+define('DB_PASSWORD', '1234');
+define('DB_DATABASE', 'oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
